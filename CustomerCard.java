38420101c5a3f366f07aeee80934a3fec45d4fd9
Naruedon-mu <marknarudon@gmail.com>
2025-02-09 22:22:18 +0700

    9/2/2568

---
+++ CustomerCard.java
@@ -0,0 +1,27 @@
+class CustomerCard extends Card {
+    private String roomOrHall;
+
+
+    public CustomerCard(String username, String id, String roomOrHall) {
+        int accessLevel = 2;
+        super(username, id, accessLevel);
+        this.roomOrHall = roomOrHall;
+    }
+
+    public String getRoomOrHall() {
+        return roomOrHall;
+    }
+
+    public void setRoomOrHall(String roomOrHall) {
+        this.roomOrHall = roomOrHall;
+    }
+
+    @Override
+    public String getCardDetails() {
+        return "Customer Card - Username: " + getUsername() +
+                ", ID: " + getId() +
+                ", Access Level: " + getAccessLevel() +
+                ", Room/Hall: " + roomOrHall;
+    }
+}
+
---
+++ EmployeeCard.java
@@ -0,0 +1,26 @@
+class EmployeeCard extends Card {
+    private String role;
+
+    // Constructor
+    public EmployeeCard(String username, String id, String role) {
+        int accessLevel = 3;
+        super(username, id, accessLevel);
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String getCardDetails() {
+        return "Employee Card - Username: " + getUsername() +
+                ", ID: " + getId() +
+                ", Access Level: " + getAccessLevel() +
+                ", Role: " + role;
+    }
+}
